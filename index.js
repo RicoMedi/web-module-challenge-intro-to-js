731be8d5f8c72188b0ad262b50b96af96a33a645
Madcodes831 <r.medina2119@gmail.com>
2023-03-12 20:54:22 -0700

    completed module 2 project

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ Do the following:
 
    HINT: no function required
 */
-
+const votingAge= (18)
+if (votingAge >= 18) {
+  console.log('Task 1a:', true);
+}else{
+  console.log('Task 1a:',false);
+}
 
 
 /*
@@ -33,6 +38,16 @@ Do the following:
 
    HINT: no function required
 */
+let cow = 'tyson';
+let attitude = 'feisty';
+
+if (attitude=== 'feisty'){
+  cow = 'betty';
+}else{
+  cow = 'tyson';
+}
+console.log('Task 1b:',cow);
+
 
 
 
@@ -48,6 +63,9 @@ Do the following:
 
    HINT: look up the Number method
 */
+const birthYear= ('1999');{
+  console.log('Task 1c:', Number(birthYear))
+}
 
 
 
@@ -61,9 +79,10 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(num1, num2){
-  return num1 * num2;
+function multiply(a, b){
+  return a * b;
 }
+console.log('Task 1d', multiply(12,12))
 
 
 
@@ -77,10 +96,11 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
-}
+function dogYears(age){
+  return age*7
 
+}
+console.log('Task 2:', dogYears(12));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -129,9 +149,25 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
         So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
-}
+function hungryDog(weight, age){
+  if (age>= 1 && weight <=5) {
+  return weight * 0.05;
+  }else if(age>=1 && weight >= 6 && weight <=10){
+    return weight * 0.04
+  }else if(age>=1 && weight >=11 && weight <= 15){
+    return weight * 0.03;
+  }else if (age >=1 && weight >15){
+    return weight * 0.02;
+  }else if (age < 1 && age >= 0.583){
+    return weight* 0.04
+  }else if (age <.583 && age>=0.333){
+    return weight * 0.05
+  }else if (age < 0.333){
+    return weight * 0.1
+  }
+  }
+  console.log('Task3:', hungryDog(15,1));
+
 
 
 
@@ -155,11 +191,32 @@ Use the game function below to do the following:
 
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
 */
-
-function game(user, computer){
-  /*add your code here*/
+let computer = Math.random();
+if (computer <= 0.34){
+  computer = 'rock';
+} else if (computer <= 0.67){
+  computer ='paper';
+}else if (computer > 0.67){
+  computer = 'scissors';
 }
+function game(user, computer){
+  if (user === computer){
+    return `it's a tie`  
+  }else if (user === 'rock' && computer ==='scissors'){
+    return `you win!`;
+  }else if (user === 'paper' && computer ==='scissors'){
+    return `you lose!`;
+  }else if(user=== 'scissors'&& computer==='paper'){
+    return `you win!`;
+  }else if(user=== 'paper'&& computer=== 'rock'){
+    return`you win!`;
+  }else{
+    return`you lose!`;
+  }
+    
+  }
 
+console.log('Task 4', game('paper', computer));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -173,10 +230,10 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(kilometers){
+return kilometers*.621371;
 }
-
+console.log('Task 5a', miles(10));
 
 
 //Task 5b - Centimeters to Feet
@@ -187,10 +244,10 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48
 }
-
+console.log('Task 5b', feet(160));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -207,10 +264,12 @@ Using the annoyingSong function below do the following:
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(number){
+     for(let i=number; i> 0; i--){
+      return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i-1} bottles of soda on the wall`;
+     } 
 }
-
+  console.log('Task 6', annoyingSong(10));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -226,47 +285,55 @@ Using the grade function below do the following:
  60-69 should return 'you got a D'
  below should return 'you got an F'
 */
-
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(number){
+  if (number >= 90){
+    return `you got an A`;
+  }else if (number < 90 && number >= 80){
+    return `you got a B`;
+  }else if (number < 80 && number >= 70){
+    return `you got a C`;
+  }else if (number < 70 && number >= 60){
+    return `you got a D`;
+  }else if (number < 60){
+    return `you got an F`;
+  }
 }
+console.log('Task 7', grade(90));
 
 
+  /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
 
-/*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
-
-//Vowel Counter - How many vowels are there?
-/*
-Using the vowelCounter function below do the following:
-1. Receive a string as a parameter
-2. Count and return the number of vowels within that string.  It should handle both capitalized and uncapitalized vowels.
-
-HINT - you may need to study tomorrow's content on arrays 
-HINT - try looking up the .includes() method
-*/
-
-
-function vowelCounter(/*add your code here*/) {
-  /*add your code here*/
-}
-
-
-
-/*🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑*/
-function foo(){
-  console.log('its working');
-  return 'bar';
-}
-foo();
-/*🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Don't touch the code after this line! 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑*/
-module.exports = {
-  foo,
-  multiply,
-  dogYears,
-  hungryDog,
-  game,
-  miles,
-  feet,
-  annoyingSong,
-  grade
-}
+  //Vowel Counter - How many vowels are there?
+  /*
+  Using the vowelCounter function below do the following:
+  1. Receive a string as a parameter
+  2. Count and return the number of vowels within that string.  It should handle both capitalized and uncapitalized vowels.
+  HINT - you may need to study tomorrow's content on arrays 
+  HINT - try looking up the .includes() method
+  */
+  
+  
+  function vowelCounter(/*add your code here*/) {
+    /*add your code here*/
+  }
+  
+  
+  
+  /*🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑*/
+  function foo(){
+    console.log('its working');
+    return 'bar';
+  }
+  foo();
+  /*🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Don't touch the code after this line! 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑*/
+  module.exports = {
+    foo,
+    multiply,
+    dogYears,
+    hungryDog,
+    game,
+    miles,
+    feet,
+    annoyingSong,
+    grade
+  }
